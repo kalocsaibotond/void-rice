74200a9b23e52237c49fd6108e359657202c4e3f
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2024-09-05 19:46:12 +0200

    feat(st): replace font2 with boxdraw, add xorg-fonts into base dependencies
    Unfortunately boxdraw conflicts with font2 and ligatures patches.

--- install_st.sh
+++ install_st.sh
@@ -8,17 +8,23 @@ sudo GIT_SSL_NO_VERIFY=$1 git clone https://git.suckless.org/st
 cd st
 sudo git checkout -b my_st
 
-# Patching
+# Downloading patches
 sudo mkdir patches
 cd patches
 if [ true = "$1" ]; then # Downloading patches
   sudo wget --no-check-certificate \
-    https://st.suckless.org/patches/font2/st-font2-0.8.5.diff
+    https://st.suckless.org/patches/boxdraw/st-boxdraw_v2-0.8.5.diff
 else
-  sudo wget https://st.suckless.org/patches/font2/st-font2-0.8.5.diff
+  sudo wget https://st.suckless.org/patches/boxdraw/st-boxdraw_v2-0.8.5.diff
 fi
 cd ..
-sudo git apply patches/st-font2-0.8.5.diff
+
+# Applying and configuring boxdraw patch
+sudo patch -p1 <patches/st-boxdraw_v2-0.8.5.diff
+sudo cp config.def.h config.h
+sudo sed -i 's/boxdraw = 0/boxdraw = 1/' config.h
+sudo sed -i 's/boxdraw_bold = 0/boxdraw_bold = 1/' config.h
+sudo sed -i 's/boxdraw_braille = 0/boxdraw_braille = 1/' config.h
 
 sudo git add -A
 sudo git commit -m "feat: setup my base st version"
