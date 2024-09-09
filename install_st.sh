eb953b89dfa5fd6f5b125d4f21ebc753ac58790c
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2024-09-09 10:06:05 +0200

    fix: repair font2 patch configuration

--- install_st.sh
+++ install_st.sh
@@ -8,7 +8,7 @@ sudo git checkout -b my_st
 printf "\nDownloading patches:\n\n"
 sudo mkdir patches
 cd patches
-if [ true = "$1" ]; then # Downloading patches
+if [ true = "$1" ]; then # Downloading patches without SSL check
   sudo wget --no-check-certificate \
     https://st.suckless.org/patches/font2/st-font2-0.8.5.diff
   sudo wget --no-check-certificate \
@@ -25,23 +25,26 @@ cd ..
 printf "\nApplying patches\n\n"
 printf "\nApplying font2 patch:\n\n"
 sudo git apply patches/st-font2-0.8.5.diff
-echo "\nApplying boxdraw patch:\n\n"
+printf "\nApplying boxdraw patch:\n\n"
 sudo patch -p1 <patches/st-boxdraw_v2-0.8.5.diff
-echo "\nApplying glyph wide support patch:\n\n"
+printf "\nApplying glyph wide support patch:\n\n"
 sudo git apply patches/st-glyph-wide-support-boxdraw-20220411-ef05519.diff
 
 printf "\nConfiguring patches\n\n"
 sudo cp config.def.h config.h
-sudo sed -i 's/boxdraw = 0/boxdraw = 1/' config.h
-sudo sed -i 's/boxdraw_bold = 0/boxdraw_bold = 1/' config.h
-sudo sed -i 's/boxdraw_braille = 0/boxdraw_braille = 1/' config.h
 
-# Configuring font2
+printf "\nConfiguring font2 patch:\n\n"
 sudo sed -i 's/Liberation Mono/'$(
 )'OpenDyslexicMono/' config.h
 sudo sed -i '/Inconsolata for Powerline/d' config.h
-sudo sed -i 's/^.\+Hack Symbols Nerd Font.\+$/'$(
-)'\t"Symbols Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",/'
+sudo sed -i 's/^.\+Hack Nerd Font Mono.\+$/'$(
+)'\t"Symbols Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",/' \
+  config.h
+
+printf "\nConfiguring boxdraw patch:\n\n"
+sudo sed -i 's/boxdraw = 0/boxdraw = 1/' config.h
+sudo sed -i 's/boxdraw_bold = 0/boxdraw_bold = 1/' config.h
+sudo sed -i 's/boxdraw_braille = 0/boxdraw_braille = 1/' config.h
 
 sudo git add -A
 sudo git commit -m "feat: setup my base st version"
