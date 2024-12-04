493e6fa89e0e8f6e329e0d6421744598e884794b
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2024-12-04 12:00:42 +0100

    fix(slstatus): correct sed configuring

--- install_slstatus.sh
+++ install_slstatus.sh
@@ -8,10 +8,10 @@ sudo git checkout -b my_slstatus
 printf "\nConfiguring slstatus\n\n"
 sudo cp config.def.h config.h
 
-sed -i "s/\\([^{]\\+\\){ datetime, \"%s\",\\([^\"]\\+\\).\\+/"$(
+sudo sed -i "s/\\([^{]\\+\\){ datetime, \"%s\",\\([^\"]\\+\\).\\+/"$(
 )"\\1{ battery_perc, \"bat: %s\",\\2\"BAT0\" },"$(
 )"\\n\\1{ keymap, \" kb: %s\",\\2NULL },"$(
-)"\\n\\1{ datetime, \" %s\",\\2\"%F %T\" },/"
+)"\\n\\1{ datetime, \" %s\",\\2\"%F %T\" },/" config.h
 
 sudo git add ./config.h
 sudo git commit -m "feat: setup my base slstatus version"
