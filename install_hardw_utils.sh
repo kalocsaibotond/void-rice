7c98199e7b915babdb80771b3032a58b0e30cb81
Botond Kalocsai <botond.kalocsai@3dhistech.com>
2025-09-27 13:53:34 +0200

    feat(hardware_utils): add Broadcom card and bluetooth installations

--- install_hardw_utils.sh
+++ install_hardw_utils.sh
@@ -9,10 +9,42 @@ else
 fi
 
 dependencies="void-repo-nonfree"
+pci_devices=$(lspci)
+usb_devices=$(lsusb)
+
+bluetooth_tools="bluez bluez-alsa"
 
 if [ -n "$(find /sys/class/power_supply/ -name [bB][aA][tT]*)" ]; then
   printf "\nAdd tlp because batteries are detected:\n\n"
   dependencies="${dependencies} tlp"
+fi
+
+if [ -n "$(echo "$pci_devices\n$usb_devices" | grep -e 'BCM431[1-3]
+BCM432[1-2]
+BCM4322[4-5]
+BCM4322[7-8]
+BCM43142
+BCM4331
+BCM4352
+BCM4360')" ]; then
+  printf "\nAdd broadcom-wl-dkms because Broadcom cards are detected:\n\n"
+  dependencies="${dependencies} broadcom-wl-dkms"
+fi
+
+if [ -n "$(echo "$pci_devices\n$usb_devices" | grep -e 'BCM2070[2-3]
+BCM43142
+BCM4335
+BCM4350
+BCM4356
+BCM4371
+BCM943142')" ]; then
+  printf "\nAdd broadcom-bt-firmware, bluez because Broadcom cards are detected:\n\n"
+  dependencies="${dependencies} broadcom-bt-firmware ${bluetooth_tools}"
+fi
+
+if [ -n "$(find /sys/class/bluetooth/ -name [hH][cH][iH]*)" ]; then
+  printf "\nAdd bluez because bluetooth devices are detected:\n\n"
+  dependencies="${dependencies} ${bluetooth_tools}"
 fi
 
 sudo $env_vars xbps-install -Sy $dependencies
